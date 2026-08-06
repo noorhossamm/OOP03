26f0153e095f081dfbe2a08c32b42941594177b9
noorhossamm <noormwafy78@gmail.com>
2026-08-06 14:20:28 +0300

    Question_01

--- Assignment_05/Program.cs
+++ Assignment_05/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Assignment_05
+﻿using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment_05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
+            #region  Theoretical Questions
+            #region Question01
+            // 1-Class is reference type stored in heap and supports inheritance while struct is value type stored in stack and does not support inheritance
+            // Class is better as it support inheritance , polymorphism and better for complex objects, it  avoid copying large amounts of data when passed 
+            #endregion 
+            #endregion
         }
     }
 }
