1ba7ae65571ff06dbc62b6513fc22af81e8d9b77
noorhossamm <noormwafy78@gmail.com>
2026-08-09 14:29:16 +0300

    Assignment 3(Constructor Chaining)

---
+++ Assignment_05/PriorityInternationalShipment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_05
+{
+    internal class PriorityInternationalShipment : InternationalShipment
+    {
+        public PriorityInternationalShipment(string trackingCode,string description,double weight,double deliveryFee, DeliveryAddress destination,string destinationCountry,decimal customsFee)
+    : base(trackingCode,description,weight,deliveryFee,destination,destinationCountry,customsFee)
+        {
+        }
+
+    }
+}
